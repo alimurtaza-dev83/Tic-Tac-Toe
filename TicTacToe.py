ed6f88fcef587a97de748c284a09ab5621d842c1
alimurtaza-dev83 <ali.murtaza.dev83@gmail.com>
2026-06-27 16:48:28 +0500

    Tic Tac Toe v1.1

--- TicTacToe.py
+++ TicTacToe.py
@@ -12,15 +12,15 @@ class Standard():
         self.blue = 0
         self.color = self.colorarr[self.count%2]
         self.root = tk.Tk()
-        self.root.geometry("700x700")
-        self.root.resizable(False, False)
+        self.root.geometry("400x400")
+        self.root.resizable(False,False)
         self.root.configure(bg = 'white')
         self.buttonarray = [[],[],[]]
         self.styles()
         for ro in range(3):
             for colum in range(3):
-                btn = ttk.Button(self.root, text = None, command = lambda r = ro, c = colum: self.disabler(r, c), style = 'stan.TButton')
-                btn.grid(row=(ro + 1), column=(colum + 1), padx=5, pady=5 ,sticky = 'nsew', ipadx=2, ipady=2)
+                btn = ttk.Button(self.root, text = None, command = lambda r = ro, c = colum: self.disabler(r, c), style = 'stan.TButton', padding = 0, width = 2)
+                btn.grid(row=(ro + 1), column=(colum + 1), ipadx=0, ipady=0)
                 self.buttonarray[ro].append(btn)
         self.start()
     def disabler(self, ro, col):
@@ -33,13 +33,12 @@ class Standard():
         if self.color == 'red':
             self.checkarray[ro][col] = 1
         self.color = self.colorarr[self.count%2]
-        self.turnlabel.config(text = 'Turn:'+str(self.count))
-        self.style.configure('Turn.TLabel', font = ('Times New Roman', 50), foreground = self.color, background = 'white')
+        self.style.map('stan.TButton', background = [('active', self.color)])
         self.wincheck()
     def wincheck(self):
         end = False
         winner = 0
-        temparr = ['Draw! ', 'Red Wins!', 'Blue Wins!']
+        temparr = ['Draw! ', 'Red Wins! ', 'Blue Wins! ']
         
         for i in range(3):
             if (self.checkarray[i][0] == self.checkarray[i][1] == self.checkarray[i][2] != 0):
@@ -76,7 +75,6 @@ class Standard():
         self.color = 'red'
         self.redlabel.config(text = str(self.red))
         self.bluelabel.config(text = str(self.blue))
-        self.turnlabel.config(text = 'Turn:'+str(self.count))
         self.checkarray = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
     def styles(self):
         self.style = ttk.Style()
@@ -85,22 +83,19 @@ class Standard():
         self.style.configure('blue.TLabel', font = ('Times New Roman', 50), foreground = 'blue', background = 'white')
         self.style.configure('Turn.TLabel', font = ('Times New Roman', 50), foreground = self.color, background = 'white')
         self.style.configure('stan.TButton', font = ('Times New Roman', 50), background = 'yellow')
-        self.style.map('stan.TButton', background = [('active', 'black'), ('pressed', 'red'), ('disabled', 'white')])
+        self.style.map('stan.TButton', background = [('active', 'red'), ('disabled', 'white')])
         self.style.configure('blue.TButton', font = ('Times New Roman', 50), background = 'yellow')
         self.style.map('blue.TButton', foreground = [('disabled', 'blue')], background = [('disabled', 'white')])
         self.style.configure('red.TButton', font = ('Times New Roman', 50), background = 'yellow')
         self.style.map('red.TButton', foreground = [('disabled', 'red')], background = [('disabled', 'white')])
     def start(self):
-        for i in range(1,4):
-            self.root.rowconfigure(i, weight=1)
-            self.root.columnconfigure(i, weight=1)
-        self.root.rowconfigure(0, weight = 2)
-        self.root.rowconfigure(4, weight = 2)
+        self.root.rowconfigure(0, weight = 1)
+        self.root.rowconfigure(6, weight = 1)
+        self.root.columnconfigure(0, weight = 1)
+        self.root.columnconfigure(4, weight = 1)
         self.redlabel = ttk.Label(self.root, text = '0', style = 'red.TLabel')
-        self.redlabel.grid(row = 2,column = 0, sticky = 'w')
+        self.redlabel.grid(row = 2,column = 0)
         self.bluelabel = ttk.Label(self.root, text = '0', style = 'blue.TLabel')
-        self.bluelabel.grid(row = 2,column = 4, sticky = 'e')
-        self.turnlabel = ttk.Label(self.root, text = 'Turn:'+str(self.count), style = 'Turn.TLabel')
-        self.turnlabel.grid(row = 0,column = 2)
+        self.bluelabel.grid(row = 2,column = 4)
         self.root.mainloop()
 a = Standard()
